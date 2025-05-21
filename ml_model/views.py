a0d838407a0c4a5db18ca6a87d200e06238d90d1
Aditya <67204555+Aditya-aot@users.noreply.github.com>
2025-05-21 17:32:03 +0530

    Add files via upload

---
+++ ml_model/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+# Create your views here.
+
+def home(request) :
+
+    return render(request, 'main_page/index.html')
+
+
+
+
+def functions(request) :
+
+    return render(request, 'main_page/fun.html')
